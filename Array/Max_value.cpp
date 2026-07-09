e2aab4f87b5b887e77d14e5b082374da0ef380ac
Harshita Sharma <harshitasharma5801@gmail.com>
2026-07-09 17:42:47 +0530

    DSA practice

---
+++ Array/Max_value.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int arr[5]={4,6,11,2,8};
+    int ans=INT_MIN;
+    for(int i=0;i<5;i++)
+    {
+        if(arr[i]>ans)
+         ans=arr[i];
+    }
+    cout<<ans;
+    return 0;
+}
